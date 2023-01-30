9ef24954a66251ecbd6774719eb8fc36a4c8f842
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2023-01-30 17:11:34 +0400

    done

--- script.js
+++ script.js
@@ -6,12 +6,33 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-const player0El = document.querySelector('.player--0')
-const player1El = document.querySelector('.player--1')
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden')
+
+
+let scores, currentScore, activePlayer, winnerEl;
+
+const init = function(){
+    scores = [0,0];
+    currentScore = 0;
+    activePlayer = 0;
+    winnerEl = true;
+
+    score1El.textContent = 0;
+    score0El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    current0El.classList.add('player--active');
+    current1El.classList.remove('player--active')
+}
+init();
+
+
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -21,13 +42,9 @@ const switchPlayer = function () {
     player1El.classList.toggle('player--active');
 }
 
-const scores = [0,0] 
-let currentScore = 0;
-let activePlayer = 0;
-
 btnRoll.addEventListener('click', function(){
+    if(winnerEl) {
     let dice = Math.trunc(Math.random() * 6)+ 1;
-    console.log(dice)
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
     if(dice !== 1){
@@ -37,21 +54,30 @@ btnRoll.addEventListener('click', function(){
     }else{
         switchPlayer()
     }
+}
     
 });
 
 
 btnHold.addEventListener('click', function(){
+    if(winnerEl) {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-    
-    
 
-
-    switchPlayer()
-    
+    if(scores[activePlayer] >= 10){
+       winnerEl = false
+       document.querySelector(`#name--${activePlayer}`).textContent = "winner"
+       document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+       document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+       diceEl.classList.add('hidden')
+    }else{
+        switchPlayer();
+    }
+}    
 })
 
+
+btnNew.addEventListener('click', init)
 
 
 
